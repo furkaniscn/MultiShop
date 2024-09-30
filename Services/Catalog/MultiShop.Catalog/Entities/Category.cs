9a3311babd7f9565c1150e06635747b1ae94c93a
furkaniscn <furkan.iscn@outlook.com>
2024-10-01 00:58:14 +0300

    ProductAndCategroyClassHasBeenCreated

--- Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class Category
     {
+        //MongoDb ilişkisel veritabanı olmadığı için Id'ler string olarak tutuluyor.
+        [BsonId]//MongoDb'de Id olduğunu belirtiyoruz.
+        [BsonRepresentation(BsonType.ObjectId)]//Benzersiz bir Id olduğunu belirttik.
+        public string CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
     }
 }
