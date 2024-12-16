07295850cdd3dc079a95c5701e6beafd17ebdbb5
Furkan İŞCAN <furkan.iscn@outlook.com>
2024-12-16 10:13:58 +0200

    ResultAndQueryForAddressHasBeenAdded

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Queries/AddressQueries/GetAddressByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries
+{
+    public class GetAddressByIdQuery
+    {
+        public int Id { get; set; }
+        //Controller'da çaığırıken constructor üzerinden çağırılacağı için constructor oluşturduk.
+        public GetAddressByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    //Dışarında bir T değeri alacak ve bu alınan T değeri class olmak zorundadır.
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        //Entity framework'te filtreleme işlemi için bir lambda işareti vardır.
+        //Func bir giriş bir çıkış değeri istiyor, giriş değeri T çıkış değeri bool'dur. filter ifadesi ise yolladığımız değeri tutar.
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+    }
+}
