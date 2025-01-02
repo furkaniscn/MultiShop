d2aee4bd83b220a528537670ad3299d9a49678eb
Furkan İŞCAN <furkan.iscn@outlook.com>
2025-01-02 14:25:30 +0200

    RegisterHasBeenRefined

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RegisterController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = userRegisterDto.UserName,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+
+            if (result.Succeeded)
+                return Ok("İşlem başarıyla tamamlandı.");
+            else
+                return Ok("İşlem sırasında bir hata oluştu!");
+        }
+    }
+}
